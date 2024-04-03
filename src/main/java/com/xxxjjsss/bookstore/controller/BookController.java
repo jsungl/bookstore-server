34a92378f8c9726020132307b7e2373a563fe4f1
jsungl <wotjd931026@gmail.com>
2024-04-04 02:46:41 +0900

    feat: change response format
    API 공통 응답 모델을 이용하여 응답 형태 변경

--- src/main/java/com/xxxjjsss/bookstore/controller/BookController.java
+++ src/main/java/com/xxxjjsss/bookstore/controller/BookController.java
@@ -1,9 +1,15 @@
 package com.xxxjjsss.bookstore.controller;
 
-import com.xxxjjsss.bookstore.domain.Book;
+import com.xxxjjsss.bookstore.dto.BookRequestDto;
+import com.xxxjjsss.bookstore.dto.BookResponseDto;
+import com.xxxjjsss.bookstore.global.RsData.ApiResponse;
 import com.xxxjjsss.bookstore.service.BookService;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,6 +18,7 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @Slf4j
+@RequestMapping("/api/books")
 public class BookController {
 
     private final BookService bookService;
@@ -19,49 +26,71 @@ public class BookController {
     /**
      * 목록조회
      */
-    @GetMapping("/books")
-    public List<Book> getAllBooks() {
-        return bookService.getAllBooks();
+    @Getter
+    @AllArgsConstructor
+    public static class BooksResponse {
+        private final List<BookResponseDto> books;
+    }
+
+
+    @GetMapping
+    public ApiResponse<BooksResponse> getAllBooks(@RequestParam(value = "query", required = false) String query) {
+
+        //요청 파라미터가 있다면 검색 서비스(기본검색 - 제목)
+        if(StringUtils.hasText(query)) {
+            List<BookResponseDto> result = bookService.findBook(query);
+            return ApiResponse.success(new BooksResponse(result));
+        }
+
+        List<BookResponseDto> result = bookService.getAllBooks();
+        return ApiResponse.success(new BooksResponse(result));
+
     }
 
     /**
      * 단건 조회
      */
-    @GetMapping("/book/{bookId}")
-    public Book getBook(@PathVariable Long bookId) {
-        return bookService.getBookById(bookId);
+    @Getter
+    @AllArgsConstructor
+    public static class BookResponse {
+        private final BookResponseDto book;
+    }
+
+    @GetMapping("/{bookId}")
+    public ApiResponse<BookResponse> getBook(@PathVariable Long bookId) {
+        BookResponseDto result = bookService.getBookById(bookId);
+        return ApiResponse.success(new BookResponse(result));
     }
 
     /**
      * 등록
+     *
+     * HTTP POST /books API를 호출하면 컨트롤러는 201 응답 코드를 반환한다.
+     * HTTP 응답 상태 코드 201은 서버에서 리소스가 생성되었음을 나타낸다.
      */
-    @PostMapping("/addBook")
-    public void addBook(@RequestBody Book book) {
-        bookService.addBook(book);
+    @PostMapping
+    public ApiResponse<BookResponse> addBook(@RequestBody BookRequestDto bookDto) {
+        BookResponseDto result = bookService.addBook(bookDto);
+        return ApiResponse.success(new BookResponse(result));
     }
 
     /**
      * 수정
      */
-    @PutMapping("/update")
-    public void updateBook(@RequestParam("bookId") Long bookId, @RequestBody Book book) {
-        bookService.updateBook(bookId, book);
+    @PutMapping("/{bookId}")
+    public ApiResponse<BookResponse> updateBook(@PathVariable Long bookId, @RequestBody BookRequestDto bookDto) {
+        BookResponseDto result = bookService.updateBook(bookId, bookDto);
+        return ApiResponse.success(new BookResponse(result));
     }
 
     /**
      * 삭제
      */
-    @DeleteMapping("/deleteBook/{bookId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @DeleteMapping("/{bookId}")
     public void deleteBook(@PathVariable Long bookId) {
         bookService.deleteBookById(bookId);
     }
 
-    /**
-     * 검색
-     * 기본 - 제목검색
-     */
-    @GetMapping("/search")
-    public List<Book> searchBook(@RequestParam("keyword") String keyword) {
-        return bookService.findBook(keyword);
-    }
+
 }
--- src/main/java/com/xxxjjsss/bookstore/service/BookService.java
+++ src/main/java/com/xxxjjsss/bookstore/service/BookService.java
@@ -1,6 +1,10 @@
 package com.xxxjjsss.bookstore.service;
 
 import com.xxxjjsss.bookstore.domain.Book;
+import com.xxxjjsss.bookstore.dto.BookRequestDto;
+import com.xxxjjsss.bookstore.dto.BookResponseDto;
+import com.xxxjjsss.bookstore.global.exception.ApiException;
+import com.xxxjjsss.bookstore.global.exception.ErrorCode;
 import com.xxxjjsss.bookstore.repository.BookRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -8,7 +12,7 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
 
 import java.util.List;
-import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -17,33 +21,95 @@ public class BookService {
 
     private final BookRepository bookRepository;
 
-    public List<Book> getAllBooks() {
-        return bookRepository.findAll();
+    /**
+     * 목록조회
+     */
+    public List<BookResponseDto> getAllBooks() {
+        List<Book> books = bookRepository.findAll();
+        return books.stream()
+                .map(book -> BookResponseDto.builder()
+                            .book(book)
+                            .build())
+                .collect(Collectors.toList());
     }
 
-    public Book getBookById(Long bookId) {
-        return bookRepository.findById(bookId).orElseThrow(() -> new IllegalArgumentException("bookId is invalid"));
+    /**
+     * 단건 조회
+     */
+
+/*
+    public BookResponseDto getBookById(Long bookId) {
+        Book book = bookRepository.findById(bookId).orElseThrow(() -> new ApiException(ErrorCode.BOOK_NOT_FOUND, "Can not find the requested resource. id=%d".formatted(bookId)));
+        return BookResponseDto.builder().book(book).build();
+    }
+*/
+
+/*
+    public BookResponseDto getBookById(Long bookId) {
+        Book book = bookRepository.findById(bookId).orElseThrow(() -> new ApiException(ErrorCode.BOOK_NOT_FOUND, "Book was not found for parameters %d".formatted(bookId)));
+
+        return BookResponseDto.builder().book(book).build();
+    }
+*/
+
+    public BookResponseDto getBookById(Long bookId) {
+        Book book = bookRepository.findById(bookId).orElseThrow(() -> new ApiException(ErrorCode.BOOK_NOT_FOUND));
+
+        return BookResponseDto.builder().book(book).build();
     }
 
-    public Book addBook(Book book) {
-        return bookRepository.save(book);
+    /**
+     * 등록
+     */
+    public BookResponseDto addBook(BookRequestDto bookDto) {
+
+        Book book = Book.builder()
+                .title(bookDto.getTitle())
+                .description(bookDto.getDescription())
+                .imageUrl(bookDto.getImageUrl())
+                .price(bookDto.getPrice())
+                .build();
+
+        Book saved = bookRepository.save(book);
+        return BookResponseDto.builder().book(saved).build();
+
     }
 
-    public void updateBook(Long bookId, Book bookDto) {
-        Book book = bookRepository.findById(bookId).orElseThrow(() -> new IllegalArgumentException("bookId is invalid"));
+    /**
+     * 수정
+     */
+    public BookResponseDto updateBook(Long bookId, BookRequestDto bookDto) {
+        Book book = bookRepository.findById(bookId).orElseThrow(() -> new ApiException(ErrorCode.BOOK_NOT_FOUND));
         book.update(bookDto);
+
+        Book saved = bookRepository.save(book);
+        return BookResponseDto.builder().book(saved).build();
     }
 
+    /**
+     * 삭제
+     */
     public void deleteBookById(Long bookId) {
-        Book book = bookRepository.findById(bookId).orElseThrow(() -> new IllegalArgumentException("bookId is invalid"));
+        Book book = bookRepository.findById(bookId).orElseThrow(() -> new ApiException(ErrorCode.BOOK_NOT_FOUND));
         bookRepository.delete(book);
     }
 
-    public List<Book> findBook(String keyword) {
+    /**
+     * 검색
+     */
+    public List<BookResponseDto> findBook(String keyword) {
+        List<Book> books;
+
         if(StringUtils.hasText(keyword)) {
-            return bookRepository.findByTitleLikeIgnoreCase("%" + keyword + "%");
+            books = bookRepository.findByTitleLikeIgnoreCase("%" + keyword + "%");
         } else {
-            return bookRepository.findAll();
+            books = bookRepository.findAll();
         }
+
+        return books.stream()
+                .map(book -> BookResponseDto.builder()
+                        .book(book)
+                        .build())
+                .collect(Collectors.toList());
     }
 }

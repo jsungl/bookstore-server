cf9fb2d78c3352d9f46bf37cecee6a0f45d39f7e
jsungl <wotjd931026@gmail.com>
2024-04-09 00:32:41 +0900

    feat: add spring security configuration
    스프링 시큐리티 설정 클래스 추가

---
+++ src/main/java/com/xxxjjsss/bookstore/global/security/SecurityConfig.java
@@ -0,0 +1,55 @@
+package com.xxxjjsss.bookstore.global.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+/**
+ * Spring Security Config 클래스
+ *
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        //csrf disable
+        http
+                .csrf((auth) -> auth.disable());
+
+        //From 로그인 방식 disable
+        http
+                .formLogin((auth) -> auth.disable());
+
+        //http basic 인증 방식 disable
+        http
+                .httpBasic((auth) -> auth.disable());
+
+        //세션 끄기
+        http
+                .sessionManagement((session) -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        //경로별 인가 작업
+        http
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/api/members/login", "/api/members").permitAll()
+                        .requestMatchers("/api/books", "/api/books/*").permitAll()
+                        .anyRequest().authenticated());
+
+        return http.build();
+    }
+
+}

a2f80f2cb103f7a3f174c4f550253793b7ebb8d9
jsungl <wotjd931026@gmail.com>
2024-04-14 23:32:28 +0900

    feat: update login request dto class
    @AllArgsConstructor, @NoArgsConstructor 추가

--- src/main/java/com/xxxjjsss/bookstore/dto/LoginRequestDto.java
+++ src/main/java/com/xxxjjsss/bookstore/dto/LoginRequestDto.java
@@ -1,9 +1,11 @@
 package com.xxxjjsss.bookstore.dto;
 
 import jakarta.validation.constraints.NotBlank;
-import lombok.Getter;
+import lombok.*;
 
 @Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class LoginRequestDto {
 
     @NotBlank

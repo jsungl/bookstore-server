887c613594c8d0e9a5996032a485113ea38e31b0
jsungl <wotjd931026@gmail.com>
2024-04-16 22:59:23 +0900

    feat: rename SecurityConfig to ApiSecurityConfig
    API 요청에 대한 스프링 시큐리티 설정

---
+++ src/main/java/com/xxxjjsss/bookstore/global/security/ApiSecurityConfig.java
@@ -0,0 +1,109 @@
+package com.xxxjjsss.bookstore.global.security;
+
+import com.xxxjjsss.bookstore.global.jwt.JwtAuthorizationFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+
+/**
+ * Spring Security Config 클래스
+ *
+ */
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+@Slf4j
+public class ApiSecurityConfig {
+
+    private final JwtAuthorizationFilter jwtAuthorizationFilter;
+    private final AuthenticationEntryPoint authenticationEntryPoint;
+    private final AccessDeniedHandler accessDeniedHandler;
+
+    //private static final String[] ALLOWED_URIS = {"/api/books", "/api/books/*"};
+
+
+    @Bean
+    public SecurityFilterChain apiFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .cors((cors) -> cors.configurationSource(new CorsConfigurationSource() {
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                        CorsConfiguration configuration = new CorsConfiguration();
+
+                        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000"));
+                        configuration.setAllowedMethods(Collections.singletonList("*"));
+                        configuration.setAllowCredentials(true);
+                        configuration.setAllowedHeaders(Collections.singletonList("*"));
+                        configuration.setMaxAge(3600L);
+
+                        //configuration.setExposedHeaders(Collections.singletonList("Authorization"));
+
+                        return configuration;
+                    }
+                }));
+
+        //csrf disable
+        http
+                .csrf((auth) -> auth.disable());
+
+        //From 로그인 방식 disable
+        http
+                .formLogin((auth) -> auth.disable());
+
+        //http basic 인증 방식 disable
+        http
+                .httpBasic((auth) -> auth.disable());
+
+        //세션 끄기
+        //기본 설정인 Session 방식은 사용하지 않고 JWT 방식을 사용하기 위한 설정
+        http
+                .sessionManagement((session) -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        //경로별 인가 작업
+        http
+                .securityMatcher("/api/**")
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers(HttpMethod.GET, "/api/books").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/books/**").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/members/register").permitAll()
+                        .requestMatchers(HttpMethod.POST,"/api/members/login").permitAll()
+                        .requestMatchers("/api/admin").hasRole("ADMIN")
+                        .anyRequest().authenticated());
+
+        //JwtAuthorizationFilter 등록
+        // JWT 인증/인가를 사용하기 위한 설정
+        http
+                .addFilterBefore(jwtAuthorizationFilter, UsernamePasswordAuthenticationFilter.class);
+
+        //spring security 인증/인가 예외처리
+        http
+                .exceptionHandling(handler -> handler
+                        .authenticationEntryPoint(authenticationEntryPoint)
+                        .accessDeniedHandler(accessDeniedHandler)
+                );
+
+
+
+        return http.build();
+    }
+
+}

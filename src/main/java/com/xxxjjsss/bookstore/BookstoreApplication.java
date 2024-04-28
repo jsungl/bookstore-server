51ee6945d697d449494dfc3065cfbb2fde4e7d98
jsungl <wotjd931026@gmail.com>
2024-04-28 23:46:37 +0900

    feat: add exclude attribute
    스프링 시큐리티 사용시 자동으로 제공되는 시큐리티 패스워드 대신 JWT를 사용하기 위해 UserDetailsServiceAutoConfiguration 클래스를 제외

--- src/main/java/com/xxxjjsss/bookstore/BookstoreApplication.java
+++ src/main/java/com/xxxjjsss/bookstore/BookstoreApplication.java
@@ -1,12 +1,15 @@
 package com.xxxjjsss.bookstore;
 
-import com.xxxjjsss.bookstore.repository.book.BookRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Profile;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication
+/**
+ * This generated password is for development use only. Your security configuration must be updated before running your application in production.
+ * 스프링 시큐리티 사용시 자동으로 제공되는 시큐리티 패스워드 대신 JWT를 사용하므로 UserDetailsServiceAutoConfiguration 클래스를 제외시킨다
+ */
+@SpringBootApplication(exclude= {UserDetailsServiceAutoConfiguration.class})
 @EnableJpaAuditing
 public class BookstoreApplication {
 
@@ -14,10 +17,12 @@ public class BookstoreApplication {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
 
-	//@Bean
-	@Profile("local")
+/*
+	@Bean
+	@Profile("dev")
 	public TestDataInit testDataInit(BookRepository bookRepository) {
 		return new TestDataInit(bookRepository);
 	}
+*/
 
 }
